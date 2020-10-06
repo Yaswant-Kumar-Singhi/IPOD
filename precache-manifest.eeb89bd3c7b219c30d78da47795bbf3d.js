7b9bae1535a682527b4e5ca839255d34441fc815
Yaswant-Kumar-Singhi <singhiyaswant@gmail.com>
2020-10-06 14:17:20 +0530

    Updates

---
+++ precache-manifest.eeb89bd3c7b219c30d78da47795bbf3d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fae688bba9860a837b2990a0fa01dddd",
+    "url": "/IPOD/index.html"
+  },
+  {
+    "revision": "3180a0f7e56de67b54a6",
+    "url": "/IPOD/static/css/main.3bcc81b5.chunk.css"
+  },
+  {
+    "revision": "5fc8dabeb2fba4fc3eeb",
+    "url": "/IPOD/static/js/2.b5a0de9b.chunk.js"
+  },
+  {
+    "revision": "3b6069e01f0db714f9e787b4dbe9775c",
+    "url": "/IPOD/static/js/2.b5a0de9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3180a0f7e56de67b54a6",
+    "url": "/IPOD/static/js/main.ad75d073.chunk.js"
+  },
+  {
+    "revision": "03518b183c4d1365de3b",
+    "url": "/IPOD/static/js/runtime-main.72397efd.js"
+  },
+  {
+    "revision": "654284e49b04cf92849c0102c75a104a",
+    "url": "/IPOD/static/media/player.654284e4.png"
+  },
+  {
+    "revision": "6d6b9f14df4592c1fadb86e3eec84090",
+    "url": "/IPOD/static/media/song.6d6b9f14.mp3"
+  }
+]);
